bc52bff414f5e52f63d342412c3243d043027596
cosimogiove <cosimo18497@gmail.com>
2024-01-26 18:05:49 +0100

    feat: main

--- resources/views/shared/main1.blade.php
+++ resources/views/shared/main1.blade.php
@@ -1,3 +1,15 @@
 <main>
-    main
+    <div class="contenitoremain">
+        <div class="container img-comics">
+            @foreach ($comics as $comic)
+                <div class="contenuto">
+                    <img src="{{ $comic['thumb'] }}" alt="img">
+                    <div>
+                        {{ $comic['title'] }}
+                    </div>
+                </div>
+            @endforeach
+        </div>
+
+    </div>
 </main>
