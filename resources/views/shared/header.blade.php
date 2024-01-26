d03035ca1aa642914810016438fea2bff840a176
cosimogiove <cosimo18497@gmail.com>
2024-01-26 16:47:23 +0100

    feat: header+jumbotron

---
+++ resources/views/shared/header.blade.php
@@ -0,0 +1,32 @@
+<header>
+    <div class="testo">
+        <div class="container">
+            DC POWER VISA ADDIOTIONAL DC SITES
+        </div>
+    </div>
+    <div class="contenitoreheader container">
+        <div>
+            <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="logo">
+        </div>
+        <div>
+            <ul>
+                <li>Characters</li>
+                <li>Comics</li>
+                <li>Movies</li>
+                <li>Tv</li>
+                <li>Games</li>
+                <li>Collectibles</li>
+                <li>Videos</li>
+                <li>Fans</li>
+                <li>News</li>
+                <li>Shop</li>
+                <li>
+                    <input type="text" placeholder="search">
+                </li>
+            </ul>
+        </div>
+    </div>
+    </div>
+    <div class="jumbotron">
+    </div>
+</header>
