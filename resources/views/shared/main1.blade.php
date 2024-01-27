f0be7bc0ec18046607f85bf5b7e223aee830b793
cosimogiove <cosimo18497@gmail.com>
2024-01-27 12:11:36 +0100

    feat: img

--- resources/views/shared/main1.blade.php
+++ resources/views/shared/main1.blade.php
@@ -7,9 +7,34 @@
                     <div>
                         {{ $comic['title'] }}
                     </div>
+                    <div>
+                        {{ $comic['price'] }}
+                    </div>
+                    <div>
+                        {{$comic["series"]}}
+                    </div>
                 </div>
             @endforeach
         </div>
-
+        <div class="bgcolor">
+        <div class=" container d-flex justify-content-between contenitoreimg align-items-center">
+            <span>
+                <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="img1">
+                <span>Digital Comics</span>
+            </span>
+            <span>
+                <img src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}" alt="img2">
+                <span>Dc Mercandise</span>
+            </span>
+            <span>
+                <img src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}" alt="img3">
+            </span>
+            <span>Shop Locator</span>
+            <span>
+                <img src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}" alt="img4">
+            </span>
+            <span>Subscription</span>
+        </div>
+    </div>
     </div>
 </main>
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ Route::get('/', function () {
     $data = [
         "comics" => config("comics")
     ];
+  
     return view('main', $data);
 });
 
